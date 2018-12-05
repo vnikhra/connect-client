a32dc65606c6b640676f752d9bcb49ef0b40a43d
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-06 05:17:00 +0530

    added basic register page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./home";
+import Register from "./register";
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/register" exact component={Register} />
     </Switch>
   </BrowserRouter>
 );
---
+++ src/routes/register.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Input, Header, Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+class Register extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmit = async() => {
+    console.log(
+      await this.props.mutate({
+        variables: this.state
+      })
+    );
+  };
+
+  render() {
+    return (
+      <Container text>
+        <Header as={"h2"}>Register</Header>
+        <Input
+          name={"username"}
+          onChange={this.onChange}
+          placeholder={"username"}
+          fluid
+        />
+        <Input
+          name={"email"}
+          onChange={this.onChange}
+          placeholder={"email"}
+          fluid
+        />
+        <Input
+          name={"password"}
+          onChange={this.onChange}
+          type={"password"}
+          placeholder={"password"}
+          fluid
+        />
+        <Button onClick={this.onSubmit}> Submit </Button>
+      </Container>
+    );
+  }
+}
+
+const registerMutation = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password)
+  }
+`;
+
+export default graphql(registerMutation)(Register);
