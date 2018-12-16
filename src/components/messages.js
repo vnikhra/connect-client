1ec8dc7754a079eff70cc880a754f9e56c389a14
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-16 20:10:10 +0530

    Displaying comments as message list.

--- src/components/messages.js
+++ src/components/messages.js
@@ -3,4 +3,6 @@ import Styled from "styled-components";
 export default Styled.div`
   grid-column: 3;
   grid-row: 2;
+  padding-left: 20px;
+  padding-right: 20px;
 `;
--- src/containers/messageContainer.js
+++ src/containers/messageContainer.js
@@ -2,13 +2,28 @@ import React from "react";
 import Messages from "../components/messages";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import { Comment } from "semantic-ui-react";
 
 const messageContainer = ({ data: { loading, messages } }) => {
   if (loading) return null;
   console.log(messages);
   return (
     <Messages>
-      {JSON.stringify(messages)}
+      <Comment.Group>
+        {messages.map(message => (
+          <Comment key={"message-" + message.id}>
+            <Comment.Content>
+              <Comment.Author as={"a"}>{message.user.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>
+                  {new Date(parseInt(message.createdAt, 10)).toString()}
+                </div>
+              </Comment.Metadata>
+              <Comment.Text>{message.message}</Comment.Text>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
     </Messages>
   );
 };
